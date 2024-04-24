f9079a5c0be1b4f9fe5244b75c31f182359b5118
yuanlin2008 <yuanlin2008@hotmail.com>
2024-04-24 13:44:31 +0000

    123

--- mix.exs
+++ mix.exs
@@ -7,14 +7,31 @@ defmodule PortServer.MixProject do
       version: "0.1.0",
       elixir: "~> 1.16",
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      deps: deps(),
+      description: description(),
+      package: package()
     ]
   end
 
   # Run "mix help compile.app" to learn about applications.
   def application do
     [
-      extra_applications: [:logger, :observer, :wx, :runtime_tools],
+      extra_applications: [:logger, :observer, :wx, :runtime_tools]
+    ]
+  end
+
+  def description do
+    "PortServer is a GenServer interface to NodeJS implementation, built on top of Erlang ports."
+  end
+
+  def package do
+    [
+      files: ["lib", "mix.exs", "README.md", "LICENSE"],
+      maintainers: ["yuanlin2008"],
+      licenses: ["MIT"],
+      links: %{
+        "GitHub" => "https://github.com/yuanlin2008/port_server"
+      }
     ]
   end
 
